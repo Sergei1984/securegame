362f167843bc46e91619d71b1625888c1cf9dba0
Serhii Tokariev <seregat1984@gmail.com>
2022-11-17 21:38:44 +0200

    More complex collisions

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,44 @@ fn setup_physics(mut commands: Commands) {
     /* Create the ground. */
     commands
         .spawn()
-        .insert(Collider::cuboid(1000.0, 50.0))
+        // .insert(Collider::cuboid(1000.0, 50.0))
+        .insert(Collider::polyline(
+            vec![
+                Vec2::new(-500.0, 100.0),
+                Vec2::new(100.0, 0.0),
+                Vec2::new(500.0, 100.0),
+            ],
+            None,
+        ))
+        .insert(Friction::coefficient(1.0))
         .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
+
+    commands
+        .spawn()
+        .insert(Collider::polyline(
+            vec![Vec2::new(50.0, 60.0), Vec2::new(200.0, 20.0)],
+            None,
+        ))
+        .insert(Friction::coefficient(1.0));
 
     /* Create the bouncing ball. */
     commands
         .spawn()
         .insert(RigidBody::Dynamic)
-        .insert(Collider::ball(50.0))
-        .insert(Restitution::coefficient(0.9))
+        .insert(Collider::ball(20.0))
+        .insert(Restitution::coefficient(1.3))
+        .insert(Friction::coefficient(1.0))
         .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
+
+    commands
+        .spawn()
+        .insert(RigidBody::Dynamic)
+        .insert(Collider::ball(20.0))
+        .insert(Restitution::coefficient(1.3))
+        .insert(Friction::coefficient(1.0))
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(
+            -10.0, 200.0, 0.0,
+        )));
 }
 
 fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
