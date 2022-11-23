13892cfb93c663a64c0df736389a8d6386e229d7
Serhii Tokariev <seregat1984@gmail.com>
2022-11-23 21:15:17 +0200

    Copy assets

--- build.rs
+++ build.rs
@@ -1,25 +1,46 @@
 use std::env;
 use std::fs::*;
 use std::path::Path;
+use std::path::PathBuf;
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("../../../assets/fonts");
 
-    let source_path = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap())
-        .join("assets/fonts/FiraMono-Medium.ttf");
+    let dst = Path::new(&out_dir).join("../../../assets/");
+    let src = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("assets/");
 
-    println!("cargo:warning=OUT_PATH is {:?}", &dest_path);
-    println!("cargo:warning=OUT_PATH is {:?}", &source_path);
-
-    if !dest_path.is_dir() {
-        let _ = create_dir_all(&dest_path);
-    }
-
-    let r = copy(source_path, dest_path.join("FiraMono-Medium.ttf"));
+    let r = copy_recursive(&src, &dst);
 
     println!("cargo:warning=COPY RESULT is {:?}", r);
 
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=assets/fonts/FiraMono-Medium.ttf");
+}
+
+fn copy_recursive(from_dir: &PathBuf, to_dir: &PathBuf) -> std::io::Result<u64> {
+    println!(
+        "cargo:warning=COPY RECURSIVE {:?} to {:?}",
+        &from_dir, &to_dir
+    );
+
+    if !to_dir.exists() {
+        create_dir_all(&to_dir)?;
+    }
+
+    for item in read_dir(from_dir)? {
+        let path = item?;
+        let dest = to_dir.join(path.file_name());
+
+        if path.file_type().unwrap().is_dir() {
+            println!("cargo:warning=CREATE DIRECTORY {:?}", &dest);
+            if !dest.exists() {
+                create_dir_all(&dest)?;
+            }
+            copy_recursive(&path.path(), &dest)?;
+        } else {
+            println!("cargo:warning=COPY FILE {:?} to {:?}", path, &dest);
+            copy(path.path(), dest)?;
+        }
+    }
+    Ok(0)
 }
